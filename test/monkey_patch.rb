bf3b511c63fd244379dd30e12e65e4d8740a51db
Ryan Waldron <rew@erebor.com>
2011-12-29 14:42:06 -0600

    Move little monkey patch out to its own file to shut up a warning that I can't otherwise explain

---
+++ test/monkey_patch.rb
@@ -0,0 +1,6 @@
+
+class Test::Unit::TestCase
+  def api_url
+    'http://sandbox.bunchball.net/nitro/json'
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,4 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 require "test/unit"
 require "bunchball"
 require "mocha"
-
-class Test::Unit::TestCase
-  def api_url
-    'http://sandbox.bunchball.net/nitro/json'
-  end
-end
+require "monkey_patch"
