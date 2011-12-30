673da6d0acfc6cfe5cae24c4106b15a68d6d5959
Ryan Waldron <rew@erebor.com>
2011-12-29 23:00:48 -0600

    Do real award_challenge test

--- test/test_user.rb
+++ test/test_user.rb
@@ -19,14 +19,24 @@ class TestUser < Test::Unit::TestCase
 
     Bunchball::Nitro::User.expects(:post).with("user.modifyUserId", params).returns(return_value)
 
-    u = Bunchball::Nitro::User.modify_user_id('wiggly', 'puggly')
-    assert_equal u, true  # want actual true here, not just a value that evals as true
+    response = Bunchball::Nitro::User.modify_user_id('wiggly', 'puggly')
+    assert_equal response, true  # want actual true here, not just a value that evals as true
   end
 
   def test_award_challenge
-    HTTParty.expects(:post).with(api_url, :body => {:method => "user.login", :userId => "winnerwinner@chickendinner.com", :apiKey => "1234"}).returns({'Nitro' => {'Login' => {'sessionKey' => 'winning'}}})
+    # Set keys instead of mocking login
+    Bunchball::Nitro.session_key = "1234"
 
-    response = Bunchball::Nitro.login("winnerwinner@chickendinner.com", "1234")
-    assert_equal "winning", response
+    params = {:userId => 'wiggly', :challenge => 'A challenge'}
+
+    return_value = {'Nitro' => {'Achievements' => {'challengesAchieved' => {'ChallengeAchieved' =>
+        {"name"=>"A challenge", "catalogItems"=>true, "pointCategory"=>"Points", "points"=>"80", "repeatable"=>"0"}
+      }}}}
+
+    Bunchball::Nitro::User.expects(:post).with("user.awardChallenge", params).returns(return_value)
+
+    response = Bunchball::Nitro::User.award_challenge('wiggly', 'A challenge')
+    assert_equal 'A challenge', response['challengesAchieved']['ChallengeAchieved']['name']
+    assert_equal '80', response['challengesAchieved']['ChallengeAchieved']['points']
   end
 end
