0b1aff7ee08f274b761b5eb0a61841455d6754ef
Ryan Waldron <rew@erebor.com>
2012-01-02 23:16:59 -0600

    Add tests for User get_points_balance (class and instance versions)

--- test/test_user.rb
+++ test/test_user.rb
@@ -40,6 +40,43 @@ class TestUser < Test::Unit::TestCase
     assert_equal response, true
   end
 
+  def test_get_points_balance
+    params = {:userId => 'wiggly'}
+
+    return_value = {'Nitro' => {'Balance' =>
+        {"pointCategories"=>
+          {"PointCategory"=>
+            {"shortName"=>"Pts",
+              "iconUrl"=>"",
+             "name"=>"Points",
+             "premium"=>"0",
+             "isDefault"=>"true",
+             "points"=>"877",
+             "lifetimeBalance"=>"1002"}},
+         "points"=>"877",
+         "userId"=>"wiggly",
+         "lifetimeBalance"=>"1002"}
+       }}
+
+    Bunchball::Nitro::User.expects(:post).with("user.getPointsBalance", params).returns(return_value)
+
+    response = Bunchball::Nitro::User.get_points_balance('wiggly')
+    assert_equal response['pointCategories']['PointCategory']['points'], '877'
+  end
+
+  # Test the instance version of the same method
+  def test_get_points_balance_instance
+    Bunchball::Nitro.expects(:authenticate).with("wibble").returns('a_session_key')
+
+    u = Bunchball::Nitro::User.new('wibble')
+
+    # Mock out the class method with the added params
+    Bunchball::Nitro::User.expects(:get_points_balance).with('wibble', :sessionKey => 'a_session_key').returns('foo')
+
+    response = u.get_points_balance
+    assert_equal response, 'foo'
+  end
+
   def test_modify_user_id
     params = {:oldUserId => 'wiggly', :newUserId => 'puggly'}
 
