2bfb0fc3e04149d4fb61d3c5660423b65fd9e4c7
Ryan Waldron <rew@erebor.com>
2012-01-03 13:56:15 -0600

    Add undocumented (in BB Wiki) deleteActionTag and updateActionTag API call wrappers

--- lib/bunchball/nitro/admin.rb
+++ lib/bunchball/nitro/admin.rb
@@ -15,6 +15,53 @@ module Bunchball
         response = post("admin.getActionTags", params)
         return response['Nitro']['tags']['Tag']
       end
+
+      # This method is undocumented in the Bunchball Wiki
+      #
+      # API returns (on success):
+      #
+      # {"Nitro" =>
+      #   {"res" => "ok", "method" => "admin.updateActionTag", "server" => "sb00.prod.bunchball.net/nitro4.2.0",
+      #    "tags" =>
+      #    {"Tag" =>
+      #      { "name" => "my_tag4", "category" => "0", "isActionTag" => "1", "rateLimit" => "3",
+      #        "id" => "2297149096", "isEditable" => "1", "serviceActionType" => "1",
+      #        "clientId" => "144464|-563|8952078810395989", "serviceType" => "1", "lowSecurity" => "1", "prefixMatch" => "0"}
+      #    },
+      #    "asyncToken" => "4-153-157-154-153-157-154-153-157"
+      #   }
+      # }
+
+      def self.update_action_tag(tag_id, params = {})
+        response = post("admin.updateActionTag", {:tagId => tag_id}.merge(params))
+        p response
+        if (response['Nitro']['res'] == 'ok') && response['Nitro']['tags']
+          return response['Nitro']['tags']['Tag']
+        else
+          return nil
+        end
+      end
+
+      # This method is undocumented in the Bunchball Wiki
+      #
+      # API returns (on success):
+      #
+      # {"Nitro" =>
+      #   {"res" => "ok", "method"=>"admin.deleteActionTag", "server"=>"sb00.prod.bunchball.net/nitro4.2.0",
+      #    "DeleteSuccess" => {"id"=>"2297149096"},
+      #    "asyncToken" => "2-182-188-182-182-188-182-182-188"
+      #   }
+      # }
+
+      def self.delete_action_tag(tag_id, params = {})
+        response = post("admin.deleteActionTag", {:tagId => tag_id}.merge(params))
+        p response
+        if (response['Nitro']['res'] == 'ok') && response['Nitro']['DeleteSuccess']
+          return response['Nitro']['DeleteSuccess']['id']
+        else
+          return nil
+        end
+      end
     end
   end
 end
