ae31699c9bc1f5e7a6513c241ec7c863c5ac5cb0
Ryan Waldron <rew@erebor.com>
2012-01-16 13:43:06 -0600

    Finish Level class

--- lib/bunchball/nitro/level.rb
+++ lib/bunchball/nitro/level.rb
@@ -5,42 +5,34 @@ class Bunchball::Nitro::Level
     @api_response = response
   end
 
-  # def start_time
-  #   Time.at(@api_response['startTime'].to_i) if @api_response['startTime']
-  # end
-  # 
-  # def end_time
-  #   Time.at(@api_response['endTime'].to_i) if @api_response['endTime']
-  # end
-  # 
-  # def date_completed
-  #   Time.at(@api_response['dateCompleted'].to_i) if self.completed?
-  # end
-  # 
-  # def has_trophy?
-  #   @api_response['fullUrl'] || @api_response['thumbUrl']
-  # end
+  def has_icon?
+    !! @api_response['iconUrl']
+  end
+
+  def description
+    @api_response['description']
+  end
+
+  def icon_url
+    @api_response['iconUrl']
+  end
+
+  # Can't call this one 'type'; let's don't obscure built-in methods (even
+  # deprecated ones).
+  def level_type
+    @api_response['type']
+  end
 
   def name
     @api_response['name']
   end
 
-  # def point_award
-  #   @api_response['pointAward'].to_i
-  # end
-  # 
-  # def trophy_url
-  #   @api_response['fullUrl']
-  # end
-  # # We really want to call it like this, but for least-surprise, we'll also have
-  # # the more closely-matches-the-API name
-  # alias full_url trophy_url
-  # 
-  # def trophy_thumb_url
-  #   @api_response['thumbUrl']
-  # end
-  # # We really want to call it like this, but for least-surprise, we'll also have
-  # # the more closely-matches-the-API name
-  # alias thumb_url trophy_thumb_url
+  def points
+    @api_response['points'].to_i
+  end
+
+  def timestamp
+    @api_response['timestamp'].to_i
+  end
 
 end
--- test/test_level.rb
+++ test/test_level.rb
@@ -15,47 +15,44 @@ class TestLevel < Test::Unit::TestCase
     Bunchball::Nitro::Level.new(api_response)
   end
 
+  def test_has_icon?
+    level = make_level(sample_api_response.merge('iconUrl' => 'foo'))
+    assert level.has_icon?
+  end
+
+  def test_description
+    level = make_level(sample_api_response.merge('description' => 'A description'))
+    assert_equal level.description, 'A description'
+  end
+
+  def test_icon_url
+    level = make_level(sample_api_response.merge('iconUrl' => 'Duke of'))
+    assert_equal level.icon_url, 'Duke of'
+  end
+
   def test_initialize
     level = make_level
     assert_equal level.class, Bunchball::Nitro::Level
   end
 
-  # def test_has_trophy?
-  #   challenge = make_challenge(sample_api_response.merge('fullUrl' => 'foo'))
-  #   assert challenge.has_trophy?
-  #   challenge = make_challenge(sample_api_response.merge('thumbUrl' => 'foo'))
-  #   assert challenge.has_trophy?
-  # end
+  def test_level_type
+    level = make_level(sample_api_response.merge('type' => 'Type A'))
+    assert_equal level.level_type, 'Type A'
+  end
 
   def test_name
     level = make_level(sample_api_response.merge('name' => 'foo'))
     assert_equal level.name, 'foo'
   end
 
-  # def test_point_award
-  #   challenge = make_challenge(sample_api_response.merge('pointAward' => '75'))
-  #   assert_equal challenge.point_award, 75
-  # end
-  # 
-  # def test_start_time
-  #   challenge = make_challenge(sample_api_response.merge('startTime' => Time.now.to_i - 300))
-  #   assert_equal challenge.start_time.class, Time
-  #   assert_equal challenge.start_time, Time.at((Time.now.to_i - 300))
-  # end
-  # 
-  # def test_end_time
-  #   challenge = make_challenge(sample_api_response.merge('endTime' => Time.now.to_i + 300))
-  #   assert_equal challenge.end_time.class, Time
-  #   assert_equal challenge.end_time.class, Time
-  # end
-  # 
-  # def test_trophy_url
-  #   challenge = make_challenge(sample_api_response.merge('fullUrl' => 'foo'))
-  #   assert_equal 'foo', challenge.trophy_url
-  # end
-  # 
-  # def test_trophy_thumb_url
-  #   challenge = make_challenge(sample_api_response.merge('thumbUrl' => 'foo'))
-  #   assert_equal 'foo', challenge.trophy_thumb_url
-  # end
+  def test_points
+    level = make_level(sample_api_response.merge('points' => '100'))
+    assert_equal level.points, 100
+  end
+
+  def test_timestamp
+    level = make_level(sample_api_response.merge('timestamp' => Time.now.to_i - 300))
+    assert_equal level.timestamp, (Time.now.to_i - 300)
+  end
+
 end
